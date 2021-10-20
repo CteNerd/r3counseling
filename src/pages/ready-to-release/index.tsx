09f39cf13137d240dadb6009c2c7dd516b335a27
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2021-10-20 14:45:59 -0500

    link out to grow therapy for scheduling.

--- src/pages/ready-to-release/index.tsx
+++ src/pages/ready-to-release/index.tsx
@@ -13,7 +13,7 @@ export default function ReadyToRelease() {
 
       {/* <style>.spwidget-button-wrapper{text-align: center}.spwidget-button{display: inline-block;padding: 12px 24px;color: #fff !important;background: #de6a26;border: 0;border-radius: 4px;font-size: 16px;font-weight: 600;text-decoration: none}.spwidget-button:hover{background: #d15913}.spwidget-button:active{color: rgba(255, 255, 255, .75) !important;box-shadow: 0 1px 3px rgba(0, 0, 0, .15) inset}</style> */}
       <div className="spwidget-button-wrapper">
-        <a
+        {/* <a
           href="https://tiffany-luke.clientsecure.me"
           className="spwidget-button"
           data-spwidget-scope-id="b429046b-2ff3-4502-9bc6-451911b33ad0"
@@ -24,6 +24,11 @@ export default function ReadyToRelease() {
           target='_blank'
         >
           Request Appointment
+        </a> */}
+        <a
+        href='https://provider.growtherapy.com/book-appointment?id=970&ref=prov'
+        target='_blank'>
+            Request Appointment
         </a>
       </div>
       <script src="https://widget-cdn.simplepractice.com/assets/integration-1.0.js"></script>
