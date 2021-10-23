1dd5d704e577bbd0b68da0315123d6dba0729bc9
Roderick C Tomlin II <34181724+rtomlin62@users.noreply.github.com>
2021-10-23 13:36:12 -0500

    try again with this script

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -48,7 +48,6 @@ export default function Home() {
           />
         </div>
       </div>
-      <script type="text/javascript" async src="//l.getsitecontrol.com/8w8momg4.js"></script>
     </div>
   );
 }
