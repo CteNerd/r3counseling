42e662e11340c2e0e9f7d5ba53ba7adf0035a277
rtomlin62 <roddytdog@yahoo.com>
2021-11-11 06:50:43 -0600

    adding in simple practice widget

--- src/App.tsx
+++ src/App.tsx
@@ -27,108 +27,106 @@ function App() {
 
   return (
     <Router>
-      <div>
-        <div className="App">
-          <header className="App-header">
-            <div>
-              <Menu
-                onClick={(e) => handleMenuClick(e.key)}
-                selectedKeys={[selectedMenuKey]}
-                mode="horizontal"
-                className="App-header-menu"
-              >
-                <Menu.Item key="home">
-                  <a className="menu-item" href="/">
-                    Home
-                  </a>
-                </Menu.Item>
-                <Menu.Item key="about">
-                  <a href="/about">About Me</a>
-                </Menu.Item>
-                <Menu.Item key="offerings">
-                  <a href="/offerings">Offerings</a>
-                </Menu.Item>
-                <Menu.Item key="contact">
-                  <a href="/ready-to-release">Ready to Release</a>
-                </Menu.Item>
-                <Menu.Item key="terms">
-                  <a href="/terms">Terms</a>
-                </Menu.Item>
-                <Menu.Item key="news">
-                  <a href="/news">News</a>
-                </Menu.Item>
-              </Menu>
-            </div>
-          </header>
-          <body className="App-body">
-            <div className="App-body-container">
-              {/* A <Switch> looks through its children <Route>s and
+      <div className="App">
+        <header className="App-header">
+          <div>
+            <Menu
+              onClick={(e) => handleMenuClick(e.key)}
+              selectedKeys={[selectedMenuKey]}
+              mode="horizontal"
+              className="App-header-menu"
+            >
+              <Menu.Item key="home">
+                <a className="menu-item" href="/">
+                  Home
+                </a>
+              </Menu.Item>
+              <Menu.Item key="about">
+                <a href="/about">About Me</a>
+              </Menu.Item>
+              <Menu.Item key="offerings">
+                <a href="/offerings">Offerings</a>
+              </Menu.Item>
+              <Menu.Item key="contact">
+                <a href="/ready-to-release">Ready to Release</a>
+              </Menu.Item>
+              <Menu.Item key="terms">
+                <a href="/terms">Terms</a>
+              </Menu.Item>
+              <Menu.Item key="news">
+                <a href="/news">News</a>
+              </Menu.Item>
+            </Menu>
+          </div>
+        </header>
+        <body className="App-body">
+          <div className="App-body-container">
+            {/* A <Switch> looks through its children <Route>s and
       renders the first one that matches the current URL. */}
-              <Switch>
-                <Route path="/about">
-                  <About />
-                </Route>
-                <Route path="/terms">
-                  <Terms />
-                </Route>
-                <Route path="/news">
-                  <News />
-                </Route>
-                <Route path="/offerings">
-                  <Offerings />
-                </Route>
-                <Route path="/ready-to-release">
-                  <ReadyToRelease />
-                </Route>
-                <Route path="/appointment-request">
-                  <AppointmentRequest />
-                </Route>
-                <Route path="/">
-                  <Home />
-                </Route>
-              </Switch>
+            <Switch>
+              <Route path="/about">
+                <About />
+              </Route>
+              <Route path="/terms">
+                <Terms />
+              </Route>
+              <Route path="/news">
+                <News />
+              </Route>
+              <Route path="/offerings">
+                <Offerings />
+              </Route>
+              <Route path="/ready-to-release">
+                <ReadyToRelease />
+              </Route>
+              <Route path="/appointment-request">
+                <AppointmentRequest />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
+        </body>
+        <footer className="App-footer">
+          <div>
+            <div className="footer-link-container">
+              <a
+                className="footer-link"
+                href="https://www.google.com/maps/place/4210+Columbia+Rd,+Augusta,+GA+30907/@33.5060819,-82.1193749,17z/data=!3m1!4b1!4m5!3m4!1s0x88f9d370a85e0bed:0xedc6d4adfbf9f6be!8m2!3d33.5060819!4d-82.1171862"
+              >
+                4210 Columbia Rd | Martinez, GA 30907
+              </a>
             </div>
-          </body>
-          <footer className="App-footer">
-            <div>
-              <div className="footer-link-container">
-                <a
-                  className="footer-link"
-                  href="https://www.google.com/maps/place/4210+Columbia+Rd,+Augusta,+GA+30907/@33.5060819,-82.1193749,17z/data=!3m1!4b1!4m5!3m4!1s0x88f9d370a85e0bed:0xedc6d4adfbf9f6be!8m2!3d33.5060819!4d-82.1171862"
-                >
-                  4210 Columbia Rd | Martinez, GA 30907
-                </a>
-              </div>
-              <div className="footer-link-container">
-                <a className="footer-link" href="tel:1-786-755-1863">
-                  PHONE: (786) 755-1863
-                </a>
-              </div>
-              <div className="footer-link-container">
-                <a
-                  className="footer-link"
-                  href="mailto:reception+Tiffany970@growtherapy.com"
-                >
-                  EMAIL US
-                </a>
-              </div>
-              <div className="footer-link-container">
-                <a
-                  className="footer-link"
-                  href="https://www.linkedin.com/in/tiffany-luke-lpc-29683119"
-                >
-                  <LinkedinOutlined />
-                </a>
-                <a
-                  className="footer-link"
-                  href="https://www.instagram.com/r3counseling"
-                >
-                  <InstagramOutlined />
-                </a>
-              </div>
+            <div className="footer-link-container">
+              <a className="footer-link" href="tel:1-786-755-1863">
+                PHONE: (786) 755-1863
+              </a>
             </div>
-          </footer>
-        </div>
+            <div className="footer-link-container">
+              <a
+                className="footer-link"
+                href="mailto:reception+Tiffany970@growtherapy.com"
+              >
+                EMAIL US
+              </a>
+            </div>
+            <div className="footer-link-container">
+              <a
+                className="footer-link"
+                href="https://www.linkedin.com/in/tiffany-luke-lpc-29683119"
+              >
+                <LinkedinOutlined />
+              </a>
+              <a
+                className="footer-link"
+                href="https://www.instagram.com/r3counseling"
+              >
+                <InstagramOutlined />
+              </a>
+            </div>
+          </div>
+        </footer>
       </div>
     </Router>
   );
--- src/pages/ready-to-release/index.tsx
+++ src/pages/ready-to-release/index.tsx
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useEffect } from "react";
+import './r3.css';
 
 export default function ReadyToRelease() {
   return (
-    <div>
+    <div id="r3">
       <h1>Ready to Release</h1>
       <p>
         Are you ready to schedule your initial free 15 minute consultations? Do
@@ -13,26 +14,26 @@ export default function ReadyToRelease() {
 
       {/* <style>.spwidget-button-wrapper{text-align: center}.spwidget-button{display: inline-block;padding: 12px 24px;color: #fff !important;background: #de6a26;border: 0;border-radius: 4px;font-size: 16px;font-weight: 600;text-decoration: none}.spwidget-button:hover{background: #d15913}.spwidget-button:active{color: rgba(255, 255, 255, .75) !important;box-shadow: 0 1px 3px rgba(0, 0, 0, .15) inset}</style> */}
       <div className="spwidget-button-wrapper">
-        {/* <a
-          href="https://tiffany-luke.clientsecure.me"
-          className="spwidget-button"
-          data-spwidget-scope-id="b429046b-2ff3-4502-9bc6-451911b33ad0"
-          data-spwidget-scope-uri="tiffany-luke"
-          data-spwidget-application-id="7c72cb9f9a9b913654bb89d6c7b4e71a77911b30192051da35384b4d0c6d505b"
-          data-spwidget-scope-global
-          data-spwidget-autobind
-          target='_blank'
-        >
-          Request Appointment
-        </a> */}
-
         <a
           href="https://provider.growtherapy.com/book-appointment?id=970&ref=prov"
           target="_blank"
         >
-          <button>Request Appointment</button>
+          <button>Grow Therapy Appt</button>
         </a>
       </div>
+
+      <a
+        href="https://tiffany-luke.clientsecure.me"
+        className="spwidget-button"
+        data-spwidget-scope-id="b429046b-2ff3-4502-9bc6-451911b33ad0"
+        data-spwidget-scope-uri="tiffany-luke"
+        data-spwidget-application-id="7c72cb9f9a9b913654bb89d6c7b4e71a77911b30192051da35384b4d0c6d505b"
+        data-spwidget-scope-global
+        data-spwidget-autobind
+        target="_blank"
+      >
+        Simple Practice Widget
+      </a>
       <script src="https://widget-cdn.simplepractice.com/assets/integration-1.0.js"></script>
     </div>
   );
