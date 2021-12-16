79348d726910d6bfbb4ecbdb4e2750c6fa4ffc87
rtomlin62 <roddytdog@yahoo.com>
2021-12-16 05:43:31 -0600

    black tfbg logo

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -46,7 +46,7 @@ export default function Home() {
           />
           <img
             className="banner-img"
-            src="https://wellcall-app-cdk.s3.amazonaws.com/TFBG-logo.jpg"
+            src="https://wellcall-app-cdk.s3.amazonaws.com/TFBG-logo-black.jpg"
           />
         </div>
       </div>
