09573dc17177c147a10481ff1a91131cdb261709
GerrinM <gerrin.murray@gmail.com>
2024-04-25 14:34:42 -0500

    on hover

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -12,8 +12,8 @@ export default function AboutMe(props: Props) {
       <h4>
         Meet the Tiffany Luke, LPC, CPCS, EMDRIA Certified EMDR provider, TCYM{" "}
       </h4>
-      <p>Hey, hey! I’m Tiffany.</p>
-      <p>
+      {/* <p>Hey, hey! I’m Tiffany.</p> */}
+      {/* <p>
         I am a Licensed Professional Counselor with over 15 years of clinical
         experience. I am a Certified Professional Clinical Supervisor, Certified
         Holistic Therapist, and EMDR Certified provider. I am Certified in the{" "}
@@ -22,8 +22,8 @@ export default function AboutMe(props: Props) {
         </a>{" "}
         and I’m also known as The Cookie Counselor. Yep, I bake to release
         stress, ground myself, and enrich my creative sweets passion.
-      </p>
-      <div>
+      </p> */}
+      {/* <div>
         <a href="https://www.thecookierelease.com">
           <img
             className="body-img-right"
@@ -37,25 +37,25 @@ export default function AboutMe(props: Props) {
           optimal health. How? I dedicate a portion of all of my sales proceeds
           towards resources for my clients here – grounding tools and workshops.
         </p>
-      </div>
-      <p>
+      </div> */}
+      {/* <p>
         I am here to empower, support and align, while being trustworthy,
         relatable, consistent and professional. I have had the privilege of
         serving in multiple clinical capacities throughout my career. I am
         passionate and committed to helping individuals heal from sexual trauma,
         explore traumatic experiences, anxiety, self-esteem, depression and
         grief.
-      </p>
-      <p>
+      </p> */}
+      {/* <p>
         While being culturally humble, I incorporate Integrative and Holistic
         modalities, in a trauma informed and person-centered environment. During
         your journey, you deserve to be supported as you release, restore, and
         redefine!
-      </p>
+      </p> */}
       <div style={{ display: props.isMobile ? "block" : "flex" }}>
         <div
           className="journey-container-img-container"
-          style={{ textAlign: props.isMobile ? "center" : "inherit" }}
+          style={{ textAlign: props.isMobile ? "center" : "inherit", position: 'relative'}}
         >
           <img
     className="journey-container-img"
@@ -65,6 +65,37 @@ export default function AboutMe(props: Props) {
     height="727"
     style={{ width: '454px', height: '727px', objectFit: 'contain', objectPosition: '50% 50%' }}
 />
+<div
+        
+        className="info-box-style"
+    >
+      <p>
+  Hey, hey! I’m Tiffany.<br /><br />
+  I am a Licensed Professional Counselor with over 15 years of clinical
+  experience. I am a Certified Professional Clinical Supervisor, Certified
+  Holistic Therapist, and EMDR Certified provider. I am Certified in the{" "}
+  <a href="https://traumaconsciousyoga.com/" target="_blank">
+    Trauma Conscious Yoga Method
+  </a>{" "}
+  and I’m also known as The Cookie Counselor. Yep, I bake to release
+  stress, ground myself, and enrich my creative sweets passion.<br /><br />
+  My hobby turned small business,{" "}
+          {<a href="https://thecookierelease.com">The Cookie Release</a>},
+          allows me the opportunity to sow seeds and enhance my community’s
+          optimal health. How? I dedicate a portion of all of my sales proceeds
+          towards resources for my clients here – grounding tools and workshops.<br /><br />
+          I am here to empower, support and align, while being trustworthy,
+        relatable, consistent and professional. I have had the privilege of
+        serving in multiple clinical capacities throughout my career. I am
+        passionate and committed to helping individuals heal from sexual trauma,
+        explore traumatic experiences, anxiety, self-esteem, depression and
+        grief.<br /><br />
+        While being culturally humble, I incorporate Integrative and Holistic
+        modalities, in a trauma informed and person-centered environment. During
+        your journey, you deserve to be supported as you release, restore, and
+        redefine!
+</p>
+    </div>
         </div>
         <div className={props.isMobile ? "col-100" : "col-66"}>
           <h4>Professional Journey at a Glance</h4>
