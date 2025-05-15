f283caf892e285bd7706fccb278dfbab0302c9c4
Roderick C Tomlin II <34181724+CteNerd@users.noreply.github.com>
2025-05-14 21:16:57 -0500

    About page enhancements

--- src/components/professionalBio/professionalBio.tsx
+++ src/components/professionalBio/professionalBio.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./professionalBio.css";
 import { Card, Col, Row } from "antd";
 import Meta from "antd/lib/card/Meta";
@@ -19,49 +19,38 @@ const ProfessionalBio: React.FC<ProfessionalBioProps> = ({
   content,
   content2,
 }) => {
-  const uniqueId = "id" + Math.random().toString(36).substring(2, 15); // Generate a unique ID for each card
+  const [showBio, setShowBio] = useState(false);
 
   return (
     <Row justify="center">
       <Col>
         <Card
-          onMouseEnter={() => {
-            const element = document.querySelector(
-              `#${uniqueId}`
-            ) as HTMLElement;
-            if (element) {
-              element.style.display = "block";
-              element.style.opacity = "1";
-            }
-            //console.log('Mouse entered', title, content,description);
-          }}
-          onMouseLeave={() => {
-            const element = document.querySelector(
-              `#${uniqueId}`
-            ) as HTMLElement;
-            if (element) {
-              element.style.display = "none";
-              element.style.opacity = "0";
-            }
-            //console.log('Mouse left', title, content, description);
-          }}
+          onMouseEnter={() => setShowBio(true)}
+          onMouseLeave={() => setShowBio(false)}
           headStyle={{ fontWeight: "bold", textAlign: "center" }}
           bodyStyle={{ textAlign: "center" }}
           hoverable
           style={{ maxWidth: 350, backgroundColor: "#e4b099" }}
-          cover={<img alt={title} src={imageSrc} height={"500px"} />}
+          cover={
+            <div className="bio-image-container">
+              <img 
+                alt={title} 
+                src={imageSrc} 
+                className="bio-image" 
+              />
+              {showBio && (
+                <div className="bio-content-overlay">
+                  {content}
+                </div>
+              )}
+            </div>
+          }
         >
           <Meta
             title={<span style={{ fontWeight: "bold" }}>{title}</span>}
             description={description}
+            className="custom-meta"
           />
-          <div
-            id={uniqueId}
-            className="info-box-style"
-            style={{ display: "none", opacity: "0" }}
-          >
-            {content}
-          </div>
         </Card>
       </Col>
       <Col>
