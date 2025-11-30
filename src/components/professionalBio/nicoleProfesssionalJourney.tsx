0172d3b296db4634ed7e1a9c0d2819bd37f340f4
Roddy Tomlin <roddytdog@yahoo.com>
2025-11-30 01:25:07 -0600

    Update Nicole's credentials to Licensed Associate Professional Counselor

--- src/components/professionalBio/nicoleProfesssionalJourney.tsx
+++ src/components/professionalBio/nicoleProfesssionalJourney.tsx
@@ -3,6 +3,9 @@ import "./professionalBio.css";
 const NicoleProfessionalJourney = (
   <div className="professional-journey">
     <p>
+      Licensed Associate Professional Counselor
+      <br />
+      <br />
       Master of Education in Counselor Education, Clinical Mental Health:
       <br />
       <strong>Augusta University</strong>
